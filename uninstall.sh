8bbad40f38a5659fb1d19ac13b652836627fb38d
Benjamin Lenherr <122442255+blenherr@users.noreply.github.com>
2023-07-09 12:40:53 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,9 +5,9 @@ DRV_NAME="cm4io-fan"
 DRV_VERSION="0.2.0"
 DRV_DIR="/usr/src/${DRV_NAME}-${DRV_VERSION}"
 
-sudo dkms uninstall DRV_NAME/DRV_VERSION
-sudo dkms remove DRV_NAME/DRV_VERSION
-sudo rm -fr DRV_DIR
+sudo dkms uninstall ${DRV_NAME}/${DRV_VERSION}
+sudo dkms remove ${DRV_NAME}/${DRV_VERSION}
+sudo rm -fr ${DRV_DIR}
 
 echo "Done!"
 exit 0
